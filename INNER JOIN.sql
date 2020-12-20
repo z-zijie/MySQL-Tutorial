3d35dc9cf482293b1c444b81a1595d42eeb15cea
ZIJIE ZHANG <z.zijie@outlook.com>
2020-12-20 05:18:29 -0600

    Extend INNER JOIN and LEFT JOIN EXAMPLES

--- INNER JOIN.sql
+++ INNER JOIN.sql
@@ -55,3 +55,73 @@ SELECT m.member_id,
        c.name AS committe
 FROM members AS m
          INNER JOIN committees AS c USING (name);
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT productCode,
+       productName,
+       textDescription
+FROM products t1
+         INNER JOIN productlines t2
+                    ON t1.productline = t2.productline;
+
+SELECT t1.orderNumber,
+       t1.status,
+       SUM(quantityOrdered * priceEach) total
+FROM orders t1
+         INNER JOIN orderdetails t2
+                    ON t1.orderNumber = t2.orderNumber
+GROUP BY orderNumber;
+
+SELECT orderNumber,
+       orderDate,
+       orderLineNumber,
+       productName,
+       quantityOrdered,
+       priceEach
+FROM orders
+         INNER JOIN
+     orderdetails USING (orderNumber)
+         INNER JOIN
+     products USING (productCode)
+ORDER BY orderNumber,
+         orderLineNumber;
+
+
+
+SELECT orderNumber,
+       orderDate,
+       customerName,
+       orderLineNumber,
+       productName,
+       quantityOrdered,
+       priceEach
+FROM orders
+         INNER JOIN orderdetails
+                    USING (orderNumber)
+         INNER JOIN products
+                    USING (productCode)
+         INNER JOIN customers
+                    USING (customerNumber)
+ORDER BY orderNumber,
+         orderLineNumber;
+
+
+SELECT orderNumber,
+       productName,
+       msrp,
+       priceEach
+FROM products p
+         INNER JOIN orderdetails o
+                    ON p.productcode = o.productcode
+                        AND p.msrp > o.priceEach
+WHERE p.productcode = 'S10_1678';
--- LEFT JOIN.sql
+++ LEFT JOIN.sql
@@ -8,3 +8,67 @@ SELECT m.member_id,
        c.name AS committee
 FROM members AS m
          LEFT JOIN committees AS c USING (name);
+
+
+
+
+
+SELECT customerNumber,
+       customerName,
+       orderNumber,
+       status
+FROM customers
+         LEFT JOIN orders USING (customerNumber);
+
+
+
+-- The LEFT JOIN clause is very useful when you want to find rows in a table that doesn’t have a matching row from another table.
+
+SELECT c.customerNumber,
+       c.customerName,
+       o.orderNumber,
+       o.status
+FROM customers c
+         LEFT JOIN orders o
+                   ON c.customerNumber = o.customerNumber
+WHERE orderNumber IS NULL;
+
+
+
+-- This example uses two LEFT JOIN clauses to join the three tables: employees, customers, and payments.
+
+SELECT lastName,
+       firstName,
+       customerName,
+       checkNumber,
+       amount
+FROM employees
+         LEFT JOIN customers ON
+    employeeNumber = salesRepEmployeeNumber
+         LEFT JOIN payments ON
+    payments.customerNumber = customers.customerNumber
+ORDER BY customerName,
+         checkNumber;
+
+-- The first LEFT JOIN returns all employees and customers who represented each employee or NULL if the employee does not in charge of any customer.
+-- The second LEFT JOIN returns payments of each customer represented by an employee or NULL if the customer has no payment.
+
+
+
+SELECT o.orderNumber,
+       customerNumber,
+       productCode
+FROM orders o
+         LEFT JOIN orderdetails
+                   USING (orderNumber)
+WHERE orderNumber = 10123;
+
+SELECT o.orderNumber,
+       customerNumber,
+       productCode
+FROM orders o
+         LEFT JOIN orderdetails d
+                   ON o.orderNumber = d.orderNumber
+                       AND o.orderNumber = 10123;
+
+-- Notice that for INNER JOIN clause, the condition in the ON clause is equivalent to the condition in the WHERE clause.
