2c3cc9ce38ad623ac22c2b96e62338ce9ac03a74
ZIJIE ZHANG <z.zijie@outlook.com>
2020-12-20 04:12:34 -0600

    added sort command examples

--- ORDER BY/SortingData.sql
+++ ORDER BY/SortingData.sql
@@ -3,3 +3,36 @@
 # ORDER BY column1 [ASC|DESC], 
 # 	   column2 [ASC|DESC],
 #	   ...;
+
+
+SELECT
+    contactLastName,
+    contactFirstName
+FROM customers
+ORDER BY
+    contactLastName DESC,
+    contactFirstName;
+
+
+SELECT orderNumber,
+       orderLineNumber,
+       quantityOrdered * priceEach
+FROM orderdetails
+ORDER BY quantityOrdered * priceEach DESC;
+
+SELECT orderNumber,
+       orderLineNumber,
+       quantityOrdered * priceEach AS subtotal
+FROM orderdetails
+ORDER BY subtotal DESC;
+
+SELECT orderNumber,
+       status
+FROM orders
+ORDER BY FIELD(status,
+    'In Process',
+    'On Hold',
+    'Cancelled',
+    'Resolved',
+    'Disputed',
+    'Shipped');
